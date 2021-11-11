f41c61a48971aed9f40e2a31e49f4f2ada2a8ed6
duc <duc@bookbotkids.com>
2021-11-11 13:07:52 +0700

    Using singleton package

--- lib/http_log_adapter.dart
+++ lib/http_log_adapter.dart
@@ -1,11 +1,14 @@
 import 'package:logger/logger.dart';
+import 'package:singleton/singleton.dart';
 
 /// An adapter to set [Logger] for this package
 ///
 /// [Logger]:(https://pub.dev/packages/logger)
 class HttpLogAdapter {
+  factory HttpLogAdapter() =>
+      Singleton.lazy(() => HttpLogAdapter._privateConstructor()).instance;
   HttpLogAdapter._privateConstructor();
-  static HttpLogAdapter shared = HttpLogAdapter._privateConstructor();
+  static HttpLogAdapter shared = HttpLogAdapter();
 
   /// Logger instance to write log, must be set before using
   Logger logger;
